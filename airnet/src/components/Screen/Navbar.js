513fc430e2de32bd8d7503e952925800500a9d4c
AnasImamshaikh <anasimamshaikh@eng.rizvi.edu.in>
2022-09-27 19:37:44 +0000

    Login and Registration Page

--- airnet/src/components/Screen/Navbar.js
+++ airnet/src/components/Screen/Navbar.js
@@ -55,7 +55,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            AIRNET
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
@@ -111,7 +111,7 @@ const ResponsiveAppBar = () => {
               textDecoration: 'none',
             }}
           >
-            LOGO
+            AIRNET
           </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
