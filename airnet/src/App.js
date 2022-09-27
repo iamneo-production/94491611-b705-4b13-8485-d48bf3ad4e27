9087a7f6955dc1b7c72386ee47d6293a618d3aff
AnasImamshaikh <anasimamshaikh@eng.rizvi.edu.in>
2022-09-27 16:48:07 +0000

    Route created

--- airnet/src/App.js
+++ airnet/src/App.js
@@ -1,32 +1,28 @@
 import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 
-
+import AddOn from './components/AddOn';
+import History from './components/History';
+import Home from './components/Home';
+import Login from './components/Login';
+import PopularPlan from './components/PopularPlan';
+import Profile from './components/Profile';
+import Registration from './components/Registration';
 
 function App() {
 
   return (
       <Router>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/PopularPlan">
-          <PopularPlan/>
-        </Route>
-        <Route exact path="/Addon">
-          <Addon/>
-        </Route>
-        <Route exact path="/History">
-          <History/>
-        </Route>
-        <Route exact path="/Registration">
-          <SignUp />
-        </Route>
-        <Route exact path="/Login">
-          <SignIn />
-        </Route>
-      </Switch>
+      <Routes>
+      <Route path="/" element={<Home />} /> 
+      <Route path="/PopularPlan" element={<PopularPlan />} />
+      <Route path="/AddOn" element={<AddOn />} />
+      <Route path="/History" element={<History />} />
+      <Route path="/Registration" element={<Registration />} />
+      <Route path="/Login" element={<Login />} />
+      </Routes>
     </Router>
   );
 }
---
+++ airnet/src/components/AddOn.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddOn(){
+    return (
+        <div>
+            <h1> AddOn</h1>
+        </div>
+    )
+
+}
+
+export default AddOn;
